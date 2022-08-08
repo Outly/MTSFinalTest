07c5919162128db98acbeb6560c0bc1e0c55fb1f
Outly <kamenschool41@yandex.ru>
2022-08-08 18:05:28 +0600

    bug fix 1

--- src/test/java/pages/DebitCardPage.java
+++ src/test/java/pages/DebitCardPage.java
@@ -16,12 +16,13 @@ public class DebitCardPage {
     private final By DEBIT_CARD_HEADING = By.cssSelector(".Wrapper-sc-6nwvzq-0.jcdBAB");
 
     public DebitCardPage checkCardNames(String cardType) {
-        $$(CARD_NAMES).shouldBe(sizeGreaterThan(0), Duration.ofSeconds(10)).stream().forEach(x -> x.getText().contains(cardType));
+        $(DEBIT_CARD_HEADING).shouldBe(visible, Duration.ofSeconds(10));
+        $$(CARD_NAMES).shouldHave(sizeGreaterThan(0), Duration.ofSeconds(10)).stream().forEach(x -> x.getText().contains(cardType));
         return this;
     }
 
     public DebitCardPage goToFirstProduct() {
-        $(DEBIT_CARD_HEADING).shouldHave(visible);
+        $(DEBIT_CARD_HEADING).shouldBe(visible, Duration.ofSeconds(10));
         $$(TAKE_CARD_BUTTON).shouldBe(sizeGreaterThan(0), Duration.ofSeconds(10))
                             .first()
                             .shouldBe(visible)
